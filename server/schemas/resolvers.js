111175c14c9a493da85309a5e2db2067604a9f16
Robert Schwartz <bschwartz23@gmail.com>
2021-07-27 16:06:48 -0700

    Create the Thought Type Definition and Resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,11 @@
-// resolver that will serve the response for the helloWorld query.
+// Require schemas
+const { User, Thought } = require("../models");
+
+// resolver that will serve the response for the query.
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello world!";
+    thoughts: async () => {
+      return Thought.find().sort({ createdAt: -1 });
     },
   },
 };

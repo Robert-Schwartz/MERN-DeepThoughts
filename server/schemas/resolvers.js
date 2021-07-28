e57d03e0b4828aeae1e65fdeeaf0d16c74f000c7
Robert Schwartz <bschwartz23@gmail.com>
2021-07-27 17:51:57 -0700

    minor code edits, clean working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,23 +1,13 @@
-// Require schemas
 const { User, Thought } = require("../models");
 
-// resolver that will serve the response for the query.
 const resolvers = {
   Query: {
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    // get a thought by __id
-    thought: async (parent, { _id }) => {
-      return Thought.findOne({ _id });
-    },
     // get all users
     users: async () => {
       return User.find()
         .select("-__v -password")
-        .populate("friends")
-        .populate("thoughts");
+        .populate("thoughts")
+        .populate("friends");
     },
     // get a user by username
     user: async (parent, { username }) => {
@@ -25,6 +15,15 @@ const resolvers = {
         .select("-__v -password")
         .populate("friends")
         .populate("thoughts");
+    },
+    // get all thoughts
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    // get a thought by __id
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
     },
   },
 };

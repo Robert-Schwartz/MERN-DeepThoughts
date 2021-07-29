0990e47afff4b4af720d5f1455beb5c5cab14f0f
Robert Schwartz <bschwartz23@gmail.com>
2021-07-29 11:57:21 -0700

    Install and Set Up Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React from "react";
+import {
+  //React component that we'll use to provide data to all of the other components
+  ApolloProvider,
+  //constructor function that will help initialize the connection to the GraphQL API server.
+  ApolloClient,
+  //enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
+  InMemoryCache,
+  //control how the Apollo Client makes a request.
+  createHttpLink,
+} from "@apollo/client";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+// components
+// ==============================================
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home';
+// Pages
+// ==============================================
+import Home from "./pages/Home";
 
+// establish a new link to the GraphQL server at its /graphql endpoint with createHttpLink()
+// ==============================================
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+//constructor to instantiate the Apollo Client instance and create the connection to the API endpoint
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// App Declaration
+// ==============================================
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    // enable our entire application to interact with our Apollo Client instance
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
+// ==============================================
 export default App;

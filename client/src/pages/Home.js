4111c946931de10067c7d1c9aaf7dc5a6dd5a364
Robert Schwartz <bschwartz23@gmail.com>
2021-07-29 12:27:47 -0700

    Integrate Apollo Hooks in home.js page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
+// useQuery Hook from Apollo Client
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
 
 const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  // optional chaining (see notes below):
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">{/* PRINT THOUGHT LIST */}</div>
       </div>
     </main>
   );
 };
 
-export default Home;
+
+/* Optional Chaining Notes:
+// no data will exist until the query to the server is finished
+if data exists, store it in the thoughts constant we just created.
+If data is undefined, then save an empty array to the thoughts component.
+*/

deb8516c7d752a3d3e6d5819924abc545e463ceb
Robert Schwartz <bschwartz23@gmail.com>
2021-07-31 00:35:26 -0700

    bug fix, auth code and me profile fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-boost";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import {setContext} from '@apollo/client/link/context'
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -13,18 +18,26 @@ import SingleThought from "./pages/SingleThought";
 import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
 
-const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem("id_token");
 
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-  },
+const httpLink = createHttpLink({
   uri: "/graphql",
 });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+    ...headers,
+    authorization: token ? `Bearer ${token}`: "",
+  }}
+
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+    });
+
 
 function App() {
   return (
